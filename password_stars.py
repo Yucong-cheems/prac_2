4efd02dddb7f6f3040c86334bbf8de698210ca3f
YUCONG <yucong.ni@my.jcu.edu.au>
2022-11-23 16:43:15 +0800

    prac2 password_stars

---
+++ password_stars.py
@@ -0,0 +1,9 @@
+password = input("Enter your password: ")
+password_length = 8
+while len(password) < password_length:
+    print("password error!")
+    password = input(f"Enter your password with minimum {password_length} characters: ")
+
+if len(password) > password_length:
+    print(len(password) * '*')
+    print("Thank you")
